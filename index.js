4de1c680571cf9dfaa8e35683b21171b0206edd7
Ethan Moffatt <ethanmoffatt@gmail.com>
2019-12-06 15:05:27 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -2,15 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const http = require('http');
 
-http.createServer((req, res) => {
-res.writeHead(200, {
-    'Content-type': 'text/plain'
-});
-    res.write('Hey');
-    res.end();
-}).listen(4000);
-
-client.login('NjUyNDU2MDE4MDIxNzExODc0.Xepfog.EZ6rNwj0fuiwq-dPkq8V6pfD-ms');
 
 client.on('ready', () => {
 console.log('The bot is ready');
@@ -21,3 +12,14 @@ if (msg.content.includes('bot')) {
     msg.reply('Did you mention me?');
 }
 });
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+});
+
+client.login(process.env.NjUyNDU2MDE4MDIxNzExODc0.Xepfog.EZ6rNwj0fuiwq-dPkq8V6pfD-ms);//BOT_TOKEN is the Client Secret
