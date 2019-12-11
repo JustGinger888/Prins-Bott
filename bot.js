11e4c0da6a2d6594e8a8f748a07d2e4b19e199ee
Ethan Moffatt <ethanmoffatt888@icloud.com>
2019-12-11 10:15:21 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@
 const Discord = require('discord.js');
 
 // this is what we're refering to. Your client.
-const client = new Discord.Client();
+const bot = new Discord.Client();
 
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
