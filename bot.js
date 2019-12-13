6cbdde95af330ebda08ff36fc0f57ddb01d0c5a6
Ethan Moffatt <ethanmoffatt888@icloud.com>
2019-12-13 15:48:59 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,29 @@
-// Load up the discord.js library
 const Discord = require('discord.js');
 
-// this is what we're refering to. Your client.
-const bot = new Discord.Client();
+const client = new Discord.Client();
 
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(ping) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
 });
-       
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
 client.login(process.env.BOT_TOKEN);
