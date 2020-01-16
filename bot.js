70ff7100217b0da5ec6379b2910c220fbd8da206
Ethan Moffatt <ethanmoffatt@gmail.com>
2020-01-16 16:33:11 +0000

    Better bot

--- bot.js
+++ bot.js
@@ -1,51 +1,32 @@
-var Discord = require('discord.io');
+var DiscordIO = require('discord.io');
 var logger = require('winston');
-var auth = require('/Users/ethan/Desktop/Computing Discord Bot/auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+// Load config.json that contains token and prefix value 
+const config = require("/Users/ethan/Desktop/Computing Discord Bot/config.json");
+
+
+// Event runs if the bot starts, and logs in, successfully.
+bot.on("ready", () => {
+  console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`); 
+  bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
 });
 
-bot.on('message', function (user, userID, channelID, message, evt) {
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
 
-            case 'purge':
-                // This command removes all messages from all users in the channel, up to 100.
-                // get the delete count, as an actual number.
-                const deleteCount = parseInt(args[0], 10);
-                
-                // Ooooh nice, combined conditions. <3
-                if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-                return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-                
-                // So we get our messages, and delete them. Simple enough, right?
-                const fetched = await message.channel.fetchMessages({limit: deleteCount});
-                message.channel.bulkDelete(fetched)
-                .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-            }
-         }
-     }
+// Event runs when the bot joins guild
+bot.on("guildCreate", guild => {
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
 });
+
+
+// Event triggers when bot is removed from guild
+bot.on("guildDelete", guild => {
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
+});
+
+
+
+bot.login(config.token);
