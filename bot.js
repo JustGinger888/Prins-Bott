f3421346b2889fb99f5ee84030397dca7bbe8559
Ethan Moffatt <ethanmoffatt@gmail.com>
2020-01-16 17:03:55 +0000

    Purge

--- bot.js
+++ bot.js
@@ -103,7 +103,21 @@ bot.on("message", async message => {
     }
 
     
-    
+    // Removes up to 100 messages from users in the channel
+    if(command === "purge") {
+        
+        // Get delete count
+        const deleteCount = parseInt(args[0], 10);
+        
+        // Checking Lenghth
+        if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+        return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+        
+        // Get messages and delete them
+        const fetched = await message.channel.fetchMessages({limit: deleteCount});
+        message.channel.bulkDelete(fetched)
+        .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+    }
 });
 
 bot.login(config.token);
