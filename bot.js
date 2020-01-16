e31f23c66aef246c638d06e4a88a1abee52228b0
Ethan Moffatt <ethanmoffatt@gmail.com>
2020-01-16 02:39:10 +0000

    upd

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 var Discord = require('discord.io');
 var logger = require('winston');
-var auth = require('./auth.json');
+var auth = require('/Users/ethan/Desktop/Computing Discord Bot/auth.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
