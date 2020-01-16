cc30736d76fb5ee4a309f4878954933ed2f62f00
Ethan Moffatt <ethanmoffatt@gmail.com>
2020-01-16 16:35:21 +0000

    Ignore

--- bot.js
+++ bot.js
@@ -9,24 +9,33 @@ const config = require("/Users/ethan/Desktop/Computing Discord Bot/config.json")
 
 // Event runs if the bot starts, and logs in, successfully.
 bot.on("ready", () => {
-  console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`); 
-  bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
+    console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`); 
+    bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
 });
 
 
 // Event runs when the bot joins guild
 bot.on("guildCreate", guild => {
-  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
+    console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+    bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
 });
 
 
 // Event triggers when bot is removed from guild
 bot.on("guildDelete", guild => {
-  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
+    console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+    bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
 });
 
 
+// Event runs on every message received from any channel or DM
+bot.on("message", async message => {
+
+    //Ignoring messages from other bots or without prefix
+    if(message.author.bot) return;
+    if(message.content.indexOf(config.prefix) !== 0) return;
+  
+  
+});
 
 bot.login(config.token);
