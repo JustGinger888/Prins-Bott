abc6d5d99fc6856d2e5e07bbd1b864ae426c3bff
Ethan Moffatt <ethanmoffatt@gmail.com>
2020-01-21 21:53:06 +0000

    pub Start

--- commands/pub.js
+++ commands/pub.js
@@ -1,10 +1,48 @@
 //Requires
-
+const pubs = [
+	'The Hobbit',
+	'The Guide Dog',
+	'The Shooting Star',
+	'The Rockstone',
+	'The Alexandra Hotel',
+	'London Road Brew House',
+	'BrewDog Southampton',
+	'The Bedford',
+	'The Giddy Bridge',
+	'The Scholars Arms',
+	"O'Neill's Southampton",
+	'Yates Southampton',
+	'The Angel',
+	'The Duke Of Wellington',
+	'The Red Lion',
+	'Platform Tavern',
+	'The White Star Tavern',
+	'The Bookshop Alehouse',
+	'The Cricketers',
+	'Revolution Southampton',
+	'The Lion',
+	'Belgium And Blues',
+	'The Vestry Restaurant and Bar',
+	'The Mayflower Village',
+	'Slug & Lettuce Southampton',
+	'104 Southampton',
+	'All Bar One Southampton',
+	'The Standing Order',
+	'Dancing Man Brewery',
+	'The Admiral Sir Lucius Curtis',
+	'Pitcher & Piano Southampton',
+	'The Chapel Arms',
+	'Orange Rooms Southampton',
+	'The Social',
+	'Heartbreakers',
+	'Kingsland Tavern'
+];
 
 module.exports.run = async (bot, message, args) => {
 
-	
-	message.channel.send();
+	const embed = new Discord.RichEmbed()
+	.addField('Generated Pub', array[Math.random()]);
+	message.channel.send(embed);
   
   };
   
