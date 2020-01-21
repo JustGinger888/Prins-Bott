dcb7c9e645a3e5afd163c8e5d4b9dcfba276c4e7
Ethan Moffatt <ethanmoffatt@gmail.com>
2020-01-21 22:34:17 +0000

    Pub Almost Done

--- commands/pub.js
+++ commands/pub.js
@@ -1,4 +1,4 @@
-//Requires
+const Discord = require('discord.js');
 const pubs = [
 	'The Hobbit',
 	'The Guide Dog',
@@ -10,7 +10,7 @@ const pubs = [
 	'The Bedford',
 	'The Giddy Bridge',
 	'The Scholars Arms',
-	"O'Neill's Southampton",
+	'ONeills Southampton',
 	'Yates Southampton',
 	'The Angel',
 	'The Duke Of Wellington',
@@ -40,10 +40,37 @@ const pubs = [
 
 module.exports.run = async (bot, message, args) => {
 
-	const embed = new Discord.RichEmbed()
-	.addField('Generated Pub', array[Math.random()]);
-	message.channel.send(embed);
-  
+	//const command = args.shift().toLowerCase();
+
+	const monthDSP = [];
+	var count = 0;
+	if (args.shift() === "month") {
+		for (let index = 0; index < 4; index++) {
+			for (let j = 0; j < monthDSP.length; j++) {
+				if (pubs[index] === monthDSP[j]) {
+					count++;
+					if (count === 2) {
+						index--;
+						count = 0;
+					}
+				}
+				else{
+					monthDSP.push(pubs[index]);
+				}
+			}
+		}
+		const embed = new Discord.RichEmbed()
+		.addField('Generated Pubs', monthDSP)
+		.setColor(0x1ae6b3);
+		message.channel.send(monthDSP);
+	}
+	else{
+		var index = Math.floor(Math.random() * pubs.length);
+		const embed = new Discord.RichEmbed()
+		.addField('Generated Pub', pubs[index])
+		.setColor(0x1ae6b3);
+		message.channel.send(embed);
+	}
   };
   
   // Help Object
