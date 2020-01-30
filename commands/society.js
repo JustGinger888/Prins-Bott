ffed0496f1ca01c23271826409f96bf3dd02d971
Ethan Moffatt <ethanmoffatt888@icloud.com>
2020-01-30 22:22:23 +0000

    Society Added

--- commands/society.js
+++ commands/society.js
@@ -3,6 +3,22 @@ const embed = new Discord.RichEmbed();
 
 module.exports.run = async (bot, message, args) => {
 
+  let society = {
+    meetingDay : "Wednesday Afternoons in JM312",
+    president : "James Whale",
+    vicePresident : "Honour Hewett",
+    treasurer : "John Hawkins",
+    monthlyPubs : "*To Be Announced*",
+    nextHackathon: "Hack The South",
+  }
+
+  embed.title('Society Information')
+  .addField('Meeting', society.meetingDay)
+  .addField('The Big 3', society.president +'\n'+ society.vicePresident +'\n'+ society.treasurer)
+  .addField('Monthly Pub List', society.monthlyPubs)
+  .addField('Next HAckathon', society.nextHackathon)
+  .setColor(0x1ae6b3);
+	message.channel.send(embed);
       
   
   };
