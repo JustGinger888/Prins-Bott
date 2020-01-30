a7915898f0a7f90d254cc79825a7dd5569593ff6
Ethan Moffatt <ethanmoffatt888@icloud.com>
2020-01-30 19:03:43 +0000

    Embeded Unmute Responses

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,17 +1,18 @@
 const fs = module.require('fs');
+const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
   // Check perms, self, rank, etc
-  if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('You do not have Permission to unmute!');
+  if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send(embed.addField(`Error`, 'You do not have Permission to unmute!').setColor(0x1ae6b3));
   const unMute = message.mentions.members.first() || message.guild.members.get(args[0]);
-  if (!unMute) return message.channel.send('You did not specify a user mention or ID!');
-  if (unMute.highestRole.position >= message.member.highestRole.position) return message.channel.send('You can not unmute a member that is equal to or higher than yourself!');
+  if (!unMute) return message.channel.send(embed.addField(`Error`, 'You did not specify a user mention or ID!').setColor(0x1ae6b3));
+  if (unMute.highestRole.position >= message.member.highestRole.position) return message.channel.send(embed.addField(`Error`, 'You can not unmute a member that is equal to or higher than yourself!').setColor(0x1ae6b3));
 
   // Check if the user has the mutedRole
   const mutedRole = message.guild.roles.find(mR => mR.name === 'Muted');
 
   // If the mentioned user or ID does not have the "mutedRole" return a message
-  if (!mutedRole || !unMute.roles.has(mutedRole.id)) return message.channel.send('This user is not muted!');
+  if (!mutedRole || !unMute.roles.has(mutedRole.id)) return message.channel.send(embed.addField(`Error`, 'This user is not muted!').setColor(0x1ae6b3));
 
   // Remove the mentioned users role "mutedRole", "muted.json", and notify command sender
   await unMute.removeRole(mutedRole);
@@ -20,7 +21,7 @@ module.exports.run = async (bot, message, args) => {
 
   fs.writeFile('./muted.json', JSON.stringify(bot.muted), err => {
     if (err) throw err;
-    message.channel.send(`I have unmuted ${unMute.user.tag}!`);
+    message.channel.send(embed.addField(`Success`, `I have unmuted ${unMute.user.tag}!`).setColor(0x1ae6b3));
   });
 };
 
