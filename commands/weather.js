a82da1ac749220478bff7f5d1fed9d1abdbdb54f
Ethan Moffatt <ethanmoffatt888@icloud.com>
2020-01-30 21:00:41 +0000

    fnl embed

--- commands/weather.js
+++ commands/weather.js
@@ -13,8 +13,10 @@ module.exports.run = async (bot, message, args) => {
 	.then(parsedWeather => {
 		const embed = new Discord.RichEmbed()
 		.addField('Weather Forcast', 
-		'Forecast   : ' +parsedWeather.weather[0].main + '\n'+
-		'Temperature: ' +parsedWeather.main.temp 
+		'Forecast    : ' +parsedWeather.weather[0].main + '\n'+
+		'Low Temp: ' +Math.round((parsedWeather.main.temp_min -273.15)) + '°C' + '\n'+
+		'High Temp: ' +Math.round((parsedWeather.main.temp_max -273.15)) + '°C' + '\n'+
+		'Current Temp: ' +Math.round((parsedWeather.main.temp -273.15)) + '°C' + '\n'
 		)
 		.setColor(0x1ae6b3);
 		message.delete().catch(vanish_=>{}); 
