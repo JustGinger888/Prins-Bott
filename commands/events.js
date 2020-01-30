5db1c58841f616f9234086972278d9e83aea9c4c
Ethan Moffatt <ethanmoffatt888@icloud.com>
2020-01-30 22:03:56 +0000

    Events final

--- commands/events.js
+++ commands/events.js
@@ -2,43 +2,62 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
 
-	const febEmbed = new Discord.RichEmbed("February 2020", 
-		"4	18:30 - 19:30	Winchester Creatives Monthly Meetup (Winchester)\n"+
-		"4	17:30 - 19:30	BCS Workshop: Onwards from the Prawn Cocktail (Southampton)\n"+
-		"10	17:30 - 19:30	BCS Workshop: Being Right is Not Good Enough (Southampton)\n"+
-		"11	09:00 - 17:00	TeenTech Manchester (Manchester)\n"+
-		"11	19:00 - 21:00	Agile South Coast Meetup (Southampton)\n"+
-		"13 12:00			GreenIT 2020 Student and Open competitions Deadline (Online)\n"+
-		"19	09:30 - 16:30	The Evolution of Story (Southampton)\n"+
-		"20	19:00 - 21:00	Developer South Coast Meetup (Southampton)")
+	const febEmbed = new Discord.RichEmbed()
+	.addField("February 2020", 
+		"4th	@18:30 - 19:30 	\nWinchester Creatives Monthly Meetup (Winchester)\n\n"+
+		"4th	@17:30 - 19:30	\nBCS Workshop: Onwards from the Prawn Cocktail (Southampton)\n\n"+
+		"10th	@17:30 - 19:30 	\nBCS Workshop: Being Right is Not Good Enough (Southampton)\n\n"+
+		"11th	@09:00 - 17:00	\nTeenTech Manchester (Manchester)\n\n"+
+		"11th	@19:00 - 21:00	\nAgile South Coast Meetup (Southampton)\n\n"+
+		"13th 	@12:00			\nGreenIT 2020 Student and Open competitions Deadline (Online)\n\n"+
+		"19th	@09:30 - 16:30	\nThe Evolution of Story (Southampton)\n\n"+
+		"20th	@19:00 - 21:00	\nDeveloper South Coast Meetup (Southampton)")
 		.setColor(0x1ae6b3);
 	
-		const marEmbed = new Discord.RichEmbed("March 2020",
-		"3	18:30 - 19:30	Winchester Creatives Monthly Meetup (Winchester)\n"+
-		"7		(Sat)10:00- (Sun)17:00		R.U. Hacking? 2020: 24-Hour Student Hackathon (Reading)\n"+
-		"10	18:30 - 20:30	Agile South Coast Southampton (Southampton)\n"+
-		"11-12 	(Wed)09:00- (Thu)17:00		Cloud and Cyber Security Expo (London)\n"+
-		"12-13 	(Thu)09:00- (Fri)17:00		DigiFest 2020 (Birmingham)\n"+
-		"13 12:00			BCS AGD SG Artefact 2020 Competition Deadline (Online)\n"+
-		"27 09:30 - 15:30	VentureFest South (Southampton)")
+		const marEmbed = new Discord.RichEmbed()
+		.addField("March 2020",
+		"3rd		@18:30 - 19:30				\nWinchester Creatives Monthly Meetup (Winchester)\n\n"+
+		"7th		@(Sat)10:00 - (Sun)17:00	\nR.U. Hacking? 2020: 24-Hour Student Hackathon (Reading)\n\n"+
+		"10th		@18:30 - 20:30				\nAgile South Coast Southampton (Southampton)\n\n"+
+		"11-12th 	@(Wed)09:00 - (Thu)17:00	\nCloud and Cyber Security Expo (London)\n\n"+
+		"12-13th	@(Thu)09:00 - (Fri)17:00	\nDigiFest 2020 (Birmingham)\n\n"+
+		"13th		@12:00						\nBCS AGD SG Artefact 2020 Competition Deadline (Online)\n\n"+
+		"27th 		@09:30 - 15:30				\nVentureFest South (Southampton)")
 		.setColor(0x1ae6b3);
 
-		const aprEmbed = new Discord.RichEmbed("April 2020",
-		"6-7 (Mon)08:30- (Tue)17:00		British Conference for Undergraduate Research (BCUR) (Leeds)\n"+
-		"7 	18:30 - 19:30	Winchester Creatives Monthly Meetup (Winchester)\n"+
-		"14	18:30 - 20:30	Agile South Coast Southampton (Southampton)")
+		const aprEmbed = new Discord.RichEmbed()
+		.addField("April 2020",
+		"6-7th 	@(Mon)08:30- (Tue)17:00		\nBritish Conference for Undergraduate Research (BCUR) (Leeds)\n\n"+
+		"7th 	@18:30 - 19:30				\nWinchester Creatives Monthly Meetup (Winchester)\n\n"+
+		"14th	@18:30 - 20:30				\nAgile South Coast Southampton (Southampton)")
 		.setColor(0x1ae6b3);
 
-		const mayEmbed = new Discord.RichEmbed("May 2020",
-		"1 	12:00			BCS e-Learning SG 2020 International Power Point Competitions Deadline (Online)\n"+
-		"5 	18:30 - 19:30	Winchester Creatives Monthly Meetup (Winchester)\n"+
-		"12	18:30 - 20:30	Agile South Coast Southampton (Southampton)")
+		const mayEmbed = new Discord.RichEmbed()
+		.addField("May 2020",
+		"1st 	@12:00			\nBCS e-Learning SG 2020 International Power Point Competitions Deadline (Online)\n\n"+
+		"5th 	@18:30 - 19:30	\nWinchester Creatives Monthly Meetup (Winchester)\n\n"+
+		"12th	@18:30 - 20:30	\nAgile South Coast Southampton (Southampton)")
 		.setColor(0x1ae6b3);
 
-		if (args.shift() === "feb") return febEmbed;
-		else if (args.shift() === "mar") return marEmbed;
-		else if (args.shift() === "apr") return aprEmbed;
-		else if (args.shift() === "may") return marEmbed;
+		if (args.shift() === "feb") 
+		{
+			message.channel.send(febEmbed);
+		}
+		else if (args.shift() === "mar") 
+		{
+			message.channel.send(marEmbed);
+		}
+		else if (args.shift() === "apr") 
+		{
+			message.channel.send(aprEmbed);
+		}
+		else if (args.shift() === "may") 
+		{
+			message.channel.send(marEmbed);
+		}
+		else{
+			message.channel.send(new Discord.RichEmbed('Error', "Invalid month"));
+		}
   
   
   };
